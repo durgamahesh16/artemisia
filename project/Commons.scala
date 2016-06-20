336a5e46a1a75b69219bdd2646042137dadf3b2f
Charles <chlr@groupon.com>
2016-06-19 17:51:09 -0700

    checkpoint 4

--- project/Commons.scala
+++ project/Commons.scala
@@ -3,9 +3,10 @@ import sbt._
 object Commons {
 
   val dependencies = Seq (
-    "com.google.guava" % "guava" % "19.0",
+     "com.google.guava" % "guava" % "19.0",
      "com.opencsv" % "opencsv" % "3.7",
-     "com.h2database" % "h2" % "1.4.191" % "test"
+     "com.h2database" % "h2" % "1.4.191" % "test",
+     "commons-io" % "commons-io" % "2.5"
   )
 
 }
--- src/main/scala/tech/artemisia/core/DocGenerator.scala
+++ src/main/scala/tech/artemisia/core/DocGenerator.scala
@@ -3,6 +3,7 @@ package tech.artemisia.core
 import java.io.File
 import java.nio.file.{Path, Paths}
 
+import org.apache.commons.io.FileUtils
 import tech.artemisia.task.Component
 import tech.artemisia.util.FileSystemUtil
 
@@ -15,6 +16,7 @@ object DocGenerator {
 
   def main(args: Array[String]) = {
     baseDir = Paths.get(args(0))
+    FileUtils.deleteDirectory(new File(FileSystemUtil.joinPath(baseDir.toString, "docs", "components")))
     getComponents foreach { case(a,b) => writeComponentDoc(a,b) }
   }
 
@@ -26,9 +28,19 @@ object DocGenerator {
   private def writeComponentDoc(componentName: String, component: Component) = {
     val fileName = s"${componentName.toLowerCase}.md"
     val filePath = FileSystemUtil.joinPath(baseDir.toString, "docs", "components", fileName)
-    FileSystemUtil.writeFile(component.doc, new File(filePath))
+    FileSystemUtil.writeFile(componentDoc(component), new File(filePath))
   }
 
+  private def componentDoc(component: Component) = {
+
+    s"""
+       ! ${component.doc}
+       !
+       ! ${component.tasks map { _.doc(component.name) } mkString (System.lineSeparator * 4) }
+       !
+     """.stripMargin('!')
+
+  }
 
 
 
